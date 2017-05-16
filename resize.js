64417f2154e9d2996fe21d8eef198657e6a7b7f3
覃壮利 <T00000586@vipshop.com>
2017-05-16 15:40:04 +0800

    add: rem with resize demo

---
+++ resize.js
@@ -0,0 +1,14 @@
+//rem设置
+(function(doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function() {
+			var clientWidth = docEl.clientWidth;
+			if (!clientWidth) return;
+			docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
+			//宽与高度
+			document.body.style.height = clientWidth * (900 / 1440) + "px"
+		};
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
